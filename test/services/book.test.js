133e862fa62d1315334cefd86b2a4ecb199965b8
Ehmad Saeed <justehmadsaeed@gmail.com>
2022-08-16 11:43:26 +0500

    feat: add prettier config and correct book service desc.

--- test/services/book.test.js
+++ test/services/book.test.js
@@ -1,6 +1,6 @@
 const app = require('../../src/app');
 
-describe('\'book\' service', () => {
+describe('book service', () => {
   it('registered the service', () => {
     const service = app.service('book');
     expect(service).toBeTruthy();
